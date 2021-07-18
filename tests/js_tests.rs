c3c308ed9d24007825509daed24a387d5cf916f1
Jake Marsden <jakemarsdenjm@gmail.com>
2021-07-18 23:53:12 +0100

    test: Dump AST on integration test failure

--- tests/js_tests.rs
+++ tests/js_tests.rs
@@ -1,4 +1,5 @@
-use jakescript::interpreter::{Eval, Interpreter, Result, Value};
+use jakescript::ast::Program;
+use jakescript::interpreter::{Error, Eval, Interpreter, Value};
 use jakescript::lexer::Lexer;
 use jakescript::parser::Parser;
 use std::fs;
@@ -11,13 +12,14 @@ fn js_tests() {
     for dir_entry in fs::read_dir("tests-js").unwrap() {
         let source_file = dir_entry.unwrap().path();
         match eval_from_source_file(&source_file) {
-            Ok(_) => {
+            Ok(..) => {
                 success_count += 1;
                 println!("JS :: [passed] {:?}", source_file);
             }
-            Err(err) => {
+            Err((err, program)) => {
                 failure_count += 1;
                 println!("JS :: [failed] {:?}: {}", source_file, err);
+                println!("{:#?}", program);
             }
         }
     }
@@ -25,16 +27,16 @@ fn js_tests() {
     assert_eq!(failure_count, 0);
 }
 
-fn eval_from_source_file(path: impl AsRef<Path>) -> Result<Value> {
+fn eval_from_source_file(path: impl AsRef<Path>) -> Result<Value, (Error, Program)> {
     let source_code = fs::read_to_string(path).unwrap();
     eval_from_source(&source_code)
 }
 
-fn eval_from_source(source_code: &str) -> Result<Value> {
+fn eval_from_source(source_code: &str) -> Result<Value, (Error, Program)> {
     let lexer = Lexer::for_str(source_code);
     let parser = Parser::for_lexer(lexer);
     let program = parser.execute();
 
     let mut it = Interpreter::default();
-    program.eval(&mut it)
+    program.eval(&mut it).map_err(|err| (err, program))
 }
