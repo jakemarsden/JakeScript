7f546b8eeee3d81514e343c3c7b3cdde7a08fe30
Jake Marsden <jakemarsdenjm@gmail.com>
2021-08-12 17:36:49 +0100

    test: Print how long tests take

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,22 +2,12 @@ use jakescript::ast::{Program, Value};
 use jakescript::interpreter::{self, Eval, Interpreter};
 use jakescript::lexer::Lexer;
 use jakescript::parser::Parser;
-use std::path::Path;
-use std::{fs, io};
 
 // dead_code: See https://github.com/rust-lang/rust/issues/46379
 #[allow(dead_code)]
 pub fn eval(ast: &Program) -> Result<Value, interpreter::Error> {
     let mut it = Interpreter::default();
     ast.eval(&mut it)
-}
-
-// dead_code: See https://github.com/rust-lang/rust/issues/46379
-#[allow(dead_code)]
-pub fn parse_from_source_file(path: impl AsRef<Path>) -> Result<Program, io::Error> {
-    let source_code = fs::read_to_string(path)?;
-    let ast = parse_from_source_code(&source_code);
-    Ok(ast)
 }
 
 // dead_code: See https://github.com/rust-lang/rust/issues/46379
--- tests/js_tests.rs
+++ tests/js_tests.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::time::{Duration, Instant};
 
 mod common;
 
@@ -6,30 +7,50 @@ mod common;
 fn js_tests() {
     let mut success_count = 0_usize;
     let mut failure_count = 0_usize;
+    let mut total_runtime = Duration::ZERO;
+
     for dir_entry in fs::read_dir("tests-js").unwrap() {
         let source_file = dir_entry.unwrap().path();
-        let ast = common::parse_from_source_file(&source_file).expect("Failed to read source file");
-        match common::eval(&ast) {
+        let source_code = fs::read_to_string(&source_file).expect("Failed to read source file");
+
+        let started_at = Instant::now();
+        let ast = common::parse_from_source_code(&source_code);
+        let result = common::eval(&ast);
+
+        let elapsed_runtime = started_at.elapsed();
+        total_runtime += elapsed_runtime;
+
+        match result {
             Ok(..) => {
                 success_count += 1;
-                println!("    [passed] {}", source_file.display());
+                println!(
+                    "    [passed] {} ({:?})",
+                    source_file.display(),
+                    elapsed_runtime
+                );
             }
             Err(err) => {
                 failure_count += 1;
-                eprintln!("    [failed] {}: {}", source_file.display(), err);
+                eprintln!(
+                    "    [failed] {} ({:?}): {}",
+                    source_file.display(),
+                    elapsed_runtime,
+                    err
+                );
                 eprintln!("{:#?}", ast);
             }
         }
     }
+
     if failure_count == 0 {
         println!(
-            "    JavaScript tests: {} passed, {} failed",
-            success_count, failure_count
+            "    JavaScript tests: {} passed, {} failed ({:?})",
+            success_count, failure_count, total_runtime
         );
     } else {
         eprintln!(
-            "    JavaScript tests: {} passed, {} failed",
-            success_count, failure_count
+            "    JavaScript tests: {} passed, {} failed ({:?})",
+            success_count, failure_count, total_runtime
         );
         panic!("At least one JavaScript test failed");
     }
