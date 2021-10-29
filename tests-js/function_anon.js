81defefcfa8ff79ce07b056f37a442a03f75a903
Jake Marsden <jakemarsdenjm@gmail.com>
2021-10-29 23:00:44 +0100

    test: Add anonymous function tests

--- tests-js/function_anon.js
+++ tests-js/function_anon.js
@@ -19,3 +19,13 @@ let higherOrderGreet = function(greetingProvider, recipientProvider) {
     return greet2(greeting, recipient);
 };
 assert higherOrderGreet(function () { return "Hullo"; }, function () { return "world"; }) === "Hullo, world!";
+
+let meaning = (function() {
+    return 42;
+})();
+assert meaning === 42;
+
+let squared = (function(number) {
+    return number * number;
+})(5);
+assert squared === 25;
