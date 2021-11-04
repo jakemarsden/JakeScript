2cb3088b1d457851dfb662407d424d5eb72e10f6
Jake Marsden <jakemarsdenjm@gmail.com>
2021-11-04 03:07:33 +0000

    enumerate: Don't require `Enumerate` to impl `EnumerateStr`

--- enumerate/src/lib.rs
+++ enumerate/src/lib.rs
@@ -1,48 +1,63 @@
-//! ### Usage
+//! # enumerate
 //!
-//! ```rust
-//! use enumerate::{Enumerate, EnumerateStr, NoSuchVariantError};
-//! use std::str::FromStr;
-//!
-//! #[derive(Enumerate, EnumerateStr, Clone, Debug, PartialEq)]
-//! #[enumerate_str(rename_all = "UPPERCASE")]
-//! enum MyEnum {
-//!     Item1,
-//!     #[enumerate_str(rename = "item2")]
-//!     Item2,
-//!     Item3,
-//! }
-//!
-//! assert_eq!(
-//!     MyEnum::enumerate(),
-//!     &[MyEnum::Item1, MyEnum::Item2, MyEnum::Item3]
-//! );
-//!
-//! assert_eq!(MyEnum::Item1.as_str(), "ITEM1");
-//! assert_eq!(MyEnum::Item2.as_str(), "item2");
-//! assert_eq!(MyEnum::Item3.as_str(), "ITEM3");
-//!
-//! assert_eq!(format!("{}", MyEnum::Item1), "ITEM1");
-//! assert_eq!(format!("{}", MyEnum::Item2), "item2");
-//! assert_eq!(format!("{}", MyEnum::Item3), "ITEM3");
-//!
-//! assert_eq!(MyEnum::from_str("ITEM1"), Ok(MyEnum::Item1));
-//! assert_eq!(MyEnum::from_str("item2"), Ok(MyEnum::Item2));
-//! assert_eq!(MyEnum::from_str("ITEM3"), Ok(MyEnum::Item3));
-//! assert_eq!(MyEnum::from_str("invalid"), Err(NoSuchVariantError));
-//! ```
+//! Traits and macros for working with `enum`s.
 
 use std::fmt;
 
 pub use enumerate_derive::*;
 
+/// ## Usage
+///
+/// ```rust
+/// use enumerate::Enumerate;
+///
+/// #[derive(Enumerate, Debug, PartialEq)]
+/// enum MyEnum {
+///     Item1,
+///     Item2,
+///     Item3,
+/// }
+///
+/// assert_eq!(
+///     MyEnum::enumerate(),
+///     &[MyEnum::Item1, MyEnum::Item2, MyEnum::Item3]
+/// );
+/// ```
 pub trait Enumerate {
     fn enumerate() -> &'static [Self]
     where
         Self: Sized;
 }
 
-pub trait EnumerateStr: Enumerate + Clone {
+/// ## Usage
+///
+/// ```rust
+/// use enumerate::{EnumerateStr, NoSuchVariantError};
+/// use std::str::FromStr;
+///
+/// #[derive(EnumerateStr, Clone, Debug, PartialEq)]
+/// #[enumerate_str(rename_all = "UPPERCASE")]
+/// enum MyEnum {
+///     Item1,
+///     #[enumerate_str(rename = "item2")]
+///     Item2,
+///     Item3,
+/// }
+///
+/// assert_eq!(MyEnum::Item1.as_str(), "ITEM1");
+/// assert_eq!(MyEnum::Item2.as_str(), "item2");
+/// assert_eq!(MyEnum::Item3.as_str(), "ITEM3");
+///
+/// assert_eq!(format!("{}", MyEnum::Item1), "ITEM1");
+/// assert_eq!(format!("{}", MyEnum::Item2), "item2");
+/// assert_eq!(format!("{}", MyEnum::Item3), "ITEM3");
+///
+/// assert_eq!(MyEnum::from_str("ITEM1"), Ok(MyEnum::Item1));
+/// assert_eq!(MyEnum::from_str("item2"), Ok(MyEnum::Item2));
+/// assert_eq!(MyEnum::from_str("ITEM3"), Ok(MyEnum::Item3));
+/// assert_eq!(MyEnum::from_str("invalid"), Err(NoSuchVariantError));
+/// ```
+pub trait EnumerateStr: Clone {
     fn as_str(&self) -> &'static str;
 }
 
